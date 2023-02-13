016fdb3025b9209a4efc93d8feb7d69410a7145c
Даниил Ряйккенен <dramshtadiy1337@gmail.com>
2023-02-13 18:20:38 +0300

    5 sprint

--- yatube/posts/s.py
+++
@@ -1,7 +0,0 @@
-import unittest
-def ultimate_answer(question):
-    """Выводит на печать переданный аргумент и возвращает строку ответа."""
-    print(f'Ваш вопрос: {question}')
-    return f'Ответ на ваш вопрос "{question}": 42' 
-def test_case_1(self):
-    self.assertIsInstance(ultimate_answer('Что делать?'), str, 'Тест провален')
